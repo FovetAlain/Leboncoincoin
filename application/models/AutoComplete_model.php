8bc67b596f0b3ab891706a6a2ff475556005ba90
FovetAlain <fovetalain@gmail.com>
2017-02-26 11:56:50 +0100

    updated model autocomplete row name

--- application/models/AutoComplete_model.php
+++ application/models/AutoComplete_model.php
@@ -11,14 +11,14 @@ class AutoComplete_model extends CI_Model
 
 	public function getData($value)
     {
-    	$this->db->select('VILLE');
-    	$this->db->select('CP');
-    	$this->db->like('VILLE', $value, 'after'); 
-    	$this->db->or_like('CP', $value, 'after');
+    	$this->db->select('ville');
+    	$this->db->select('codePostal');
+    	$this->db->like('ville', $value, 'after'); 
+    	$this->db->or_like('codePostal', $value, 'after');
         $query = $this->db->get('cp_autocomplete'); 
         if($query->num_rows() > 0){
       		foreach ($query->result_array() as $row){
-        		$row_set[] = $row['CP'] . ' ' . $row['VILLE']; //build an array
+        		$row_set[] = $row['codePostal'] . ' ' . $row['ville']; //build an array
       		}
       		echo json_encode($row_set); //format the array into json data
     	}
